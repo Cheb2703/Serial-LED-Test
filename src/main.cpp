bd0b84dd2aa9b54b880ba761cd5e675d74f64c22
Cheb2703 <149884197+Cheb2703@users.noreply.github.com>
2025-09-26 13:31:24 -0700

    Final Edit

--- src/main.cpp
+++ src/main.cpp
@@ -2,7 +2,7 @@
 #include <Adafruit_NeoPixel.h>
 
 #define LED_PIN     25      // Pin where LED strip is connected
-#define LED_COUNT   144     // Number of LEDs in your strip
+#define LED_COUNT   60     // Number of LEDs in your strip
 int R = 0;
 int G = 0;
 int B = 255;
@@ -10,28 +10,33 @@ int B = 255;
 Adafruit_NeoPixel strip(LED_COUNT, LED_PIN, NEO_GRB + NEO_KHZ800);
 
 void setup() {
-  Serial.begin(115200);   // Start Serial communication
+  Serial.begin(115200);      // Start Serial communication
   strip.begin();
-  strip.show(); // Initialize all LEDs to 'off'
-  strip.setBrightness(255);  // Set brightness level 0-255
-  delay(1000);            // Give the Serial Monitor time to connect
+  strip.show();              // Initialize all LEDs to 'off'
+  strip.setBrightness(20);  // Set brightness level 0-255
+  delay(1000);               // Give the Serial Monitor time to connect
 }
 
 void loop() {
   // Go through each LED one by one
+
   for (int i = 0; i < LED_COUNT; i++) {
     strip.clear();              // Turn off all LEDs
     strip.setPixelColor(i, strip.Color(R, G, B)); // Red (R,G,B)
     strip.show();               // Send updated color to strip
-    delay(2);                 // Pause between LEDs
+    delay(5);                   // Pause between LEDs
   }
 
-  for (int i = LED_COUNT ; i > 0 ; i--) {
+
+  for (int i = LED_COUNT - 1 ; i >= 0 ; i--) {
     strip.clear();              // Turn off all LEDs
     strip.setPixelColor(i, strip.Color(R, G, B)); // Red (R,G,B)
     strip.show();               // Send updated color to strip
-    delay(10);                 // Pause between LEDs
+    delay(5);                   // Pause between LEDs
   }
+
+Serial.println("Done");
+  
 
   /*
   for (int i = 1; i <= 50; i++) {
